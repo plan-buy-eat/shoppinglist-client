e7282c11604db82f96794488d2080e0b1355c789
Alexander Turevskiy <3716384+oltur@users.noreply.github.com>
2024-01-02 18:39:32 +0100

    list context fixed

--- src/list.tsx
+++ src/list.tsx
@@ -1,47 +1,51 @@
 import { useMediaQuery, Theme } from "@mui/material";
-import {InfiniteList, Datagrid, TextField, EmailField, TextInput, UrlField, useListContext} from "react-admin";
+import {InfiniteList, Datagrid, TextField, TextInput} from "react-admin";
+import { WithListContext } from 'react-admin';
 
 const postFilters = [
-    <TextInput key="q" source="q" label="Search" alwaysOn />,
+    <TextInput name="q" key="q" source="q" label="Search" alwaysOn />,
 ];
 
 const CheckField = () => <span><input className="" type="checkbox"></input></span>;
 CheckField.defaultProps = { label: 'Selected' };
 
-const postRowSx = (record, index)=> {
-    const { selectedIds } = useListContext();
-    if (selectedIds.includes(record.id)) {
-        return ({ textDecoration: 'line-through' });
-    }
-    return ({ textDecoration: '' });
-};
+const getPostRowSx = (selectedIds: any[])=> {
+    return (record: any/*, index: number*/) => {
+        if (selectedIds.includes(record.id)) {
+            return ({textDecoration: 'line-through'});
+        }
+        return ({textDecoration: ''});
+    };
+}
 
 export const DataList = () => {
     const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
     return (
         <InfiniteList filters={postFilters}>
-            {isSmall ? (
-                <Datagrid 
-                    rowClick="toggleSelection"
-                    rowSx={postRowSx}
-                >
-                    <TextField source="id" />
-                    <CheckField label="Bought" />
-                    <TextField source="name" />
-                </Datagrid>
-            ) : (
-                <Datagrid 
-                    rowClick="toggleSelection"
-                    rowSx={postRowSx}
-                >
-                    {/*<TextField source="id" />*/}
-                    <CheckField label="Bought" />
-                    <TextField source="title" />
-                    <TextField source="amount" />
-                    <TextField source="unit" />
-                    <TextField source="shop" />
-                </Datagrid>
-            )}
+            <WithListContext render={({ selectedIds }) =>
+                isSmall ? (
+                    <Datagrid
+                        rowClick="toggleSelection"
+                        rowSx={getPostRowSx(selectedIds)}
+                    >
+                        <TextField source="id"/>
+                        <CheckField label="Bought"/>
+                        <TextField source="name"/>
+                    </Datagrid>
+                ) : (
+                    <Datagrid
+                        rowClick="toggleSelection"
+                        rowSx={getPostRowSx(selectedIds)}
+                    >
+                        {/*<TextField source="id" />*/}
+                        <CheckField label="Bought"/>
+                        <TextField source="title"/>
+                        <TextField source="amount"/>
+                        <TextField source="unit"/>
+                        <TextField source="shop"/>
+                    </Datagrid>
+                )}
+            />
         </InfiniteList>
     );
 };
